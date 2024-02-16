958e5265d0ba736360036f88a7f15acbcb6d988e
mingderwang <admin@ming-ders-MacBook.local>
2024-02-16 14:04:47 +0800

    v0.0.2 ok for create account from private key using viem

--- index.ts
+++ index.ts
@@ -1,15 +1,29 @@
-import { randomBytes } from 'crypto';
 import { generatePrivateKey, privateKeyToAccount, signMessage } from "viem/accounts"
+import { writeFileSync } from "fs-extra"
+import { createWalletClient, http } from 'viem'
+import { mainnet } from 'viem/chains'
 
 const main = async () => {
-const generate32ByteValue = () => {
-  return randomBytes(32).toString('hex');
-};
-console.log('0x'+generate32ByteValue());
-console.log(generatePrivateKey());
+  const privateKey =
+        (process.env.PRIVATE_KEY as Hex) ??
+        (() => {
+                const pk = generatePrivateKey()
+                writeFileSync(".envrc", `export PRIVATE_KEY=${pk}`)
+                return pk
+        })()
+
+  console.log(privateKey)
+  const account = privateKeyToAccount(privateKey)
+  console.log(account)
+
+  const client = createWalletClient({
+  account,
+  chain: mainnet,
+  transport: http()
+})
 }
   
-  main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-  });
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
