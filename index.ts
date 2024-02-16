1bc97c4c2fca71a60cfa006e77319189bcc11a9a
mingderwang <admin@ming-ders-MacBook.local>
2024-02-16 13:40:49 +0800

    use viem

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import { randomBytes } from 'crypto';
+import { generatePrivateKey, privateKeyToAccount, signMessage } from "viem/accounts"
 
 const main = async () => {
 const generate32ByteValue = () => {
   return randomBytes(32).toString('hex');
 };
-console.log(generate32ByteValue());
+console.log('0x'+generate32ByteValue());
+console.log(generatePrivateKey());
 }
   
   main().catch((error) => {
